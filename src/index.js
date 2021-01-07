e13382a7c5d1cc98e03a2738fdb090c842a539ad
ber01 <ber01@naver.com>
2021-01-08 00:34:35 +0900

    chore: update

---
+++ src/index.js
@@ -0,0 +1,47 @@
+var minkh = /** @class */ (function () {
+    function minkh() {
+    }
+    Object.defineProperty(minkh.prototype, "min", {
+        get: function () {
+            return "민경환";
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(minkh.prototype, "ha", {
+        get: function () {
+            return "하상엽";
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(minkh.prototype, "lim", {
+        get: function () {
+            return "임동훈";
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(minkh.prototype, "shin", {
+        get: function () {
+            return "신무곤";
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(minkh.prototype, "yang", {
+        get: function () {
+            return "양기석";
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(minkh.prototype, "all", {
+        get: function () {
+            return ['민경환', '하상엽', '임동훈', '신무곤', '양기석'];
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return minkh;
+}());
